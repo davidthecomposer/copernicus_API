420a0144a099e83670483f3754c779e34bbeb745
David Campbell <davidthecomposer@gmail.com>
2020-07-31 12:28:40 -0600

    initial commit

---
+++ app.mjs
@@ -0,0 +1,96 @@
+import express from "express";
+import mongoose from "mongoose";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import bodyParser from "body-parser";
+import { MusicTrack, TrackComment } from "./models/musicTracks.mjs";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const connectAndListen = async () => {
+	const localLinkToDB = "mongodb://localhost:27017/copernicusDB";
+
+	try {
+		await mongoose.connect(process.env.DB_URI || localLinkToDB, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			useFindAndModify: false,
+		});
+
+		app.listen(process.env.PORT || 3000, () => {
+			console.log(`server started on port 3000.`);
+		});
+	} catch (err) {
+		console.log(err);
+	}
+};
+
+connectAndListen();
+
+app.route("/tracks").get((req, res) => {
+	MusicTrack.find({})
+		.populate("comments")
+
+		.exec((err, trackInfo) => {
+			if (err) {
+				res.json(err);
+			} else {
+				res.json(trackInfo.map((item) => item.toObject()));
+			}
+		});
+});
+
+app.route("/tracks/:trackTitle").get((req, res) => {
+	const trackTitle = req.params.trackTitle;
+	MusicTrack.findOne({ title: trackTitle })
+		.populate("comments")
+		.exec((err, trackInfo) => {
+			if (err) {
+				res.json(err);
+			} else {
+				res.json(trackInfo.toObject({ virtuals: true }));
+			}
+		});
+});
+
+app.route("/tracks/:trackTitle/comments").post((req, res) => {
+	const data = req.body;
+	TrackComment.create(
+		{
+			track_id: data.track_id,
+			name: data.name,
+			comment: data.comment,
+			timestamp: data.timestamp,
+		},
+		(err) => {
+			if (err) {
+				res.json(err);
+			} else {
+				res.json("comment submitted");
+			}
+		}
+	);
+});
+
+app.route("/tracks/:trackTitle/comments/:commentID").delete((req, res) => {
+	const commentID = req.params.commentID;
+	const clientPassword = req.body.password;
+	const serverPassword = "apples";
+	if (clientPassword === serverPassword) {
+		TrackComment.findByIdAndDelete(commentID, (err) => {
+			if (err) {
+				res.json(err);
+			} else {
+				res.json(`Comment deleted `);
+			}
+		});
+	} else {
+		res.json("incorrect admin password");
+	}
+});
---
+++ models/musicTracks.mjs
@@ -0,0 +1,78 @@
+import mongoose from "mongoose";
+import pkg from "mongoose";
+const { Schema } = pkg;
+
+const options = { toJson: { virtuals: true }, toObject: { virtuals: true } };
+
+const musicTracksSchema = new mongoose.Schema(
+	{
+		composer: {
+			type: String,
+			required: true,
+		},
+		title: {
+			type: String,
+			required: true,
+		},
+		year: {
+			type: String,
+			required: true,
+		},
+		instrument: {
+			type: String,
+		},
+		length: {
+			type: String,
+			required: true,
+		},
+		difficulty: {
+			type: String,
+		},
+		harmony: {
+			type: String,
+		},
+		rhythm: {
+			type: String,
+		},
+		character: {
+			type: String,
+		},
+		artistDescription: {
+			type: String,
+			required: true,
+		},
+	},
+	options
+);
+
+const trackCommentsSchema = new mongoose.Schema({
+	track_id: {
+		type: String,
+		required: true,
+	},
+	name: {
+		type: String,
+		required: true,
+	},
+	comment: {
+		type: String,
+		required: true,
+	},
+	timestamp: {
+		type: Number,
+		required: true,
+	},
+});
+
+musicTracksSchema.virtual("comments", {
+	ref: "TrackComment", // The model to use
+	localField: "_id", // Find people where `localField`
+	foreignField: "track_id", // is equal to `foreignField`
+	// If `justOne` is true, 'members' will be a single doc as opposed to
+	// an array. `justOne` is false by default.
+	justOne: false,
+	options: { sort: { timestamp: -1 } }, // Query options, see http://bit.ly/mongoose-query-options
+});
+
+export const MusicTrack = mongoose.model("MusicTrack", musicTracksSchema);
+export const TrackComment = mongoose.model("TrackComment", trackCommentsSchema);
